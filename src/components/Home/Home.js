595c193ffc4f38a5853c2168357d31971ef6a262
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-11-19 14:43:46 +0100

    changed name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> Adefeyitimi G. Adeyeloja</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
