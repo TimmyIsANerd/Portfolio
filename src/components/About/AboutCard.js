c82e1ffafa7839538ed9c2706ef8584aa080df63
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-11-19 21:01:46 +0100

    changes to text

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,30 +8,30 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hello there, I am <span className="purple">Adefeyitimi Adeyeloja </span>
+            from <span className="purple"> Nigeria.</span>
+            <br />Since beginning my journey as a Web <span className="purple">Developer</span>, 
+            I've done remote jobs for <span className="purple">agencies</span>, consulted for <span className="purple">startups</span>, and collaborated with the most talented people to create digital products for both business and customer use. In my free time, I write on <span className="purple">Medium</span> , I'm always looking to improve my <span className="purple">skills</span> having one tech problem solved at a time.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
-          <ul>
+          <ul> 
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Video Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Writing Tech Blogs
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Traveling
             </li>
           </ul>
 
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "The way to get started is to quit talking and begin doing."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Walt Disney</footer>
         </blockquote>
       </Card.Body>
     </Card>
