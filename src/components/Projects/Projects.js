6ac99b4568647e17f4d1aed14c90e7b51e936e87
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-12-01 15:19:26 +0100

    updated changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,6 +14,11 @@ import myjobselfie from "../../Assets/Projects/Portfolio/MyJobSelfie/index.html.
 import provest from "../../Assets/Projects/Portfolio/Provest/provest_lg.png";
 import nomsycares from "../../Assets/Projects/Portfolio/NomsyCares/index.html.png";
 
+// Web3 Projects
+import bsctoken from "../../Assets/Projects/bsctoken_project.png";
+import rariblenft from '../../Assets/Projects/rariblenft.png';
+import nftfightresult from "../../Assets/Projects/nftresultminter.png";
+import waveportal from "../../Assets/Projects/waveportal.png";
 
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpeg";
@@ -28,8 +33,63 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Recent{" "}
+          <strong className="purple">Web3 (Blockchain) Projects </strong>
         </h1>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bsctoken}
+              isBlog={false}
+              title="Binance Smart Chain Token Deployed (Test Net) "
+              description="
+              Created BSC Token
+              TimmyToken (TTKN)
+              Using Solidity & OpenZeppelin
+
+              Contract Address @Deployment
+              0x48e2c7f28c652ed33ac04f41e8c7b1d0cc61f352
+              "
+              link="https://gist.github.com/TimmyIsANerd/72104e60d8f4c77a8ac891c925f5a554"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={rariblenft}
+              isBlog={false}
+              title="Rarible NFT Minter (Test Net)"
+              description="
+              NFT Minter Application Built and Hosted to Mint NFTs using Rarible Plugin and Moralis Js instantly publish to Rarable NFT Market Place.
+              "
+              link="https://rariblenftminter.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={nftfightresult}
+              isBlog={false}
+              title="Anime/MCU Fight Results NFT Collection"
+              description="
+              NFT Minter Project of Mine, randomly generates an NFT with random unique text characters and deploys it to OpenSea.io, I wrote the smart contract to only permit a total of 50 NFTs to be generated in total
+              "
+              link="https://rariblenftminter.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={waveportal}
+              isBlog={false}
+              title="Blockchain Wave Portal"
+              description="
+              Wave portal project, I built it to let people leave me a message via the solidity smart contract and it has a feature that is something of a lottery system that awards Fake ethereum to any random user.
+              "
+              link="http://comesayhello.herokuapp.com/"
+            />
+          </Col>
+        </Row>
+        <h2 className="project-heading">
+          My Recent <strong className="purple">Web2 Projects </strong>
+        </h2>
         <p style={{ color: "white" }}>
           I have a few projects on here I've worked on <br />
           Want to see more?{" "}
@@ -106,7 +166,7 @@ function Projects() {
               isBlog={false}
               title="Excelhence Hub"
               description="ExcelHence Hub is a growing community of global experts whose essence is global leadership and expertise that thrives on KNOWLEDGE, ATTITUDE , and SKILLS. The community is dedicated to solving the social problem of unemployment through viability trainings and the communication of perpetual cultures such as teamwork, creativity and ultimately excellence which is achieved by its accountability structure that hinges on practical systems of training and mentorship."
-              link="https://provest.pro"
+              link="#"
             />
           </Col>
 
@@ -116,7 +176,7 @@ function Projects() {
               isBlog={false}
               title="NomsyCares Foundation"
               description="Nomsy Cares Foundation is a registered non profit organisation and Non Governmental Organisation (NGO) They plan and carry out projects relating to the girl child to accomplish their objectives."
-              link="https://provest.pro"
+              link="#"
             />
           </Col>
 
@@ -125,8 +185,18 @@ function Projects() {
               imgPath={myjobselfie}
               isBlog={false}
               title="MyJobSelfie"
-              description="ExcelHence Hub is a growing community of global experts whose essence is global leadership and expertise that thrives on KNOWLEDGE, ATTITUDE , and SKILLS. The community is dedicated to solving the social problem of unemployment through viability trainings and the communication of perpetual cultures such as teamwork, creativity and ultimately excellence which is achieved by its accountability structure that hinges on practical systems of training and mentorship."
-              link="https://provest.pro"
+              description="MyJobSelfie is a platform built with the intent to lessen the need for in person interviews with prospective employees. It is responsive and entirely built using the Bootstrap Library"
+              link="#"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={provest}
+              isBlog={false}
+              title="Provest"
+              description="Provest allows you to invest [according to your capacity] in real estate easily. Your returns on investment is paid every week. Provestors earn between 20% to 50% returns within a four month contract span, and still have the option of withdrawing their capital or reinvesting it."
+              link="#"
             />
           </Col>
         </Row>
