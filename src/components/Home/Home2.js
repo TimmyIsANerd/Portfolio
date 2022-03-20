f6f11696c13e8e070eb3d620b8bc1f6c5e38d669
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-20 01:03:30 +0100

    fixed typo

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,7 +19,7 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I design and code beautifully solutions for businesses, and I love
+              I design and code beautiful solutions for businesses, and I love
               what I do.
               <br />
               <br />I am proficient at coding with
@@ -31,10 +31,10 @@ function Home2() {
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interests includes building with new &nbsp;
               <i>
                 <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                especially in areas related to{" "}
                 <b className="purple">Blockchain Development.</b>
               </i>
               <br />
